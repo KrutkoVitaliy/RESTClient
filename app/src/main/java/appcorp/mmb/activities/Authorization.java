befc19538fd99d162787181d10c6817b756b3b37
Виталий <vetla1495@gmail.com>
2016-08-13 09:37:58 +0300

    gps:base

--- app/src/main/java/appcorp/mmb/activities/Authorization.java
+++ app/src/main/java/appcorp/mmb/activities/Authorization.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -14,8 +16,10 @@ import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -29,10 +33,11 @@ import appcorp.mmb.R;
 import appcorp.mmb.classes.Storage;
 import appcorp.mmb.network.GetRequest;
 
-public class Authorization extends Activity {
+public class Authorization extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
-    TextView logoText;
-    GoogleApiClient mGoogleApiClient;
+    private TextView logoText;
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
 
     private static final int RC_SIGN_IN = 9001;
 
@@ -44,24 +49,20 @@ public class Authorization extends Activity {
         logoText = (TextView) findViewById(R.id.logoText);
         logoText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Galada.ttf"));
 
-        auth();
-    }
+        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
 
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
-
-    private void auth() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                //.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
-        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
         signInButton.setSize(SignInButton.SIZE_WIDE);
         signInButton.setScopes(gso.getScopeArray());
-        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 signIn();
@@ -69,8 +70,18 @@ public class Authorization extends Activity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
     private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
@@ -98,6 +109,9 @@ public class Authorization extends Activity {
             finish();
             //startActivity(new Intent(getApplicationContext(), MyProfile.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
         } else {
+            Status status = result.getStatus();
+            int statusCode = status.getStatusCode();
+            Toast.makeText(this, "Failed to sign in: " + statusCode, Toast.LENGTH_LONG).show();
             Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
         }
     }
