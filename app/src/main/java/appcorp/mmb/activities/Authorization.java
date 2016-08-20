d96369866c7aae466feb189c984aa02605ecd8e9
administrator <vetal1495@gmail.com>
2016-08-20 15:22:08 +0300

    Firebase Auth

--- app/src/main/java/appcorp/mmb/activities/Authorization.java
+++ app/src/main/java/appcorp/mmb/activities/Authorization.java
@@ -1,61 +1,55 @@
 package appcorp.mmb.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import appcorp.mmb.R;
-import appcorp.mmb.classes.Storage;
-import appcorp.mmb.network.GetRequest;
 
-public class Authorization extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class Authorization extends AppCompatActivity implements View.OnClickListener {
 
     private TextView logoText;
-    private GoogleApiClient googleApiClient;
-    private SignInButton signInButton;
-
-    private static final int RC_SIGN_IN = 9001;
+    private TextView signIn;
+    private EditText email, pass;
+    private Button submit;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduction);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
         logoText = (TextView) findViewById(R.id.logoText);
         logoText.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/Galada.ttf"));
-        signInButton = (SignInButton) findViewById(R.id.sign_in_button);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        googleApiClient = new GoogleApiClient
-                .Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-        signInButton.setScopes(gso.getScopeArray());
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
+        signIn = (TextView) findViewById(R.id.signIn);
+        email = (EditText) findViewById(R.id.emailField);
+        pass = (EditText) findViewById(R.id.passField);
+        submit = (Button) findViewById(R.id.submit);
+
+        signIn.setOnClickListener(this);
+        submit.setOnClickListener(this);
     }
 
     @Override
@@ -64,43 +58,41 @@ public class Authorization extends FragmentActivity implements GoogleApiClient.O
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
+    public void onClick(View view) {
+        if(view == submit) {
+            registerUser();
+        }
+        if (view == signIn){
+            /*registerUser();*/
         }
     }
 
-    private void handleSignInResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
-            GoogleSignInAccount acct = result.getSignInAccount();
-            Storage.addString("E-mail", acct.getEmail());
-            Storage.addString("Name", acct.getDisplayName());
-            Storage.addString("PhotoURL", acct.getPhotoUrl().toString());
-            String name = "" + acct.getDisplayName().replace(" ", "%20");
-            String email = "" + acct.getEmail();
-            String photo = "" + acct.getPhotoUrl().toString();
+    private void registerUser() {
+        String sEmail = email.getText().toString().trim();
+        String sPass = pass.getText().toString().trim();
 
-            new GetRequest("http://195.88.209.17/app/in/user.php?photo=" + photo + "&email=" + email).execute();
-
-            finish();
-            startActivity(new Intent(getApplicationContext(), MyProfile.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        } else {
-            Status status = result.getStatus();
-            int statusCode = status.getStatusCode();
-            Toast.makeText(this, "Failed to sign in: " + statusCode, Toast.LENGTH_LONG).show();
-            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        if(TextUtils.isEmpty(sEmail)) {
+            Toast.makeText(getApplicationContext(), "Please enter email", Toast.LENGTH_SHORT).show();
+            return;
         }
+        if(TextUtils.isEmpty(sPass)) {
+            Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(sEmail, sPass)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    startActivity(new Intent(getApplicationContext(), MyProfile.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
