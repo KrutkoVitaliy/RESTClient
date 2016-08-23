dec8566d8fdd278e01ac9dd2f8d6f86bb4938247
Виталий <vetla1495@gmail.com>
2016-08-23 11:09:56 +0300

    Encode fix

--- app/src/main/java/appcorp/mmb/activities/Authorization.java
+++ app/src/main/java/appcorp/mmb/activities/Authorization.java
@@ -3,15 +3,14 @@ package appcorp.mmb.activities;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,13 +19,23 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import appcorp.mmb.R;
+import appcorp.mmb.classes.Intermediates;
+import appcorp.mmb.classes.Storage;
+import appcorp.mmb.network.GetRequest;
 
 public class Authorization extends AppCompatActivity implements View.OnClickListener {
 
     private TextView logoText;
     private TextView signIn;
-    private EditText email, pass;
+    private EditText email, pass, name, lastname;
     private Button submit;
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
@@ -46,6 +55,8 @@ public class Authorization extends AppCompatActivity implements View.OnClickList
         signIn = (TextView) findViewById(R.id.signIn);
         email = (EditText) findViewById(R.id.emailField);
         pass = (EditText) findViewById(R.id.passField);
+        name = (EditText) findViewById(R.id.nameField);
+        lastname = (EditText) findViewById(R.id.lastnameField);
         submit = (Button) findViewById(R.id.submit);
 
         signIn.setOnClickListener(this);
@@ -59,40 +70,60 @@ public class Authorization extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        if(view == submit) {
+        if (view == submit) {
             registerUser();
         }
-        if (view == signIn){
+        if (view == signIn) {
             /*registerUser();*/
         }
     }
 
     private void registerUser() {
-        String sEmail = email.getText().toString().trim();
-        String sPass = pass.getText().toString().trim();
+        final String sEmail = Intermediates.encodeToURL(email.getText().toString().trim());
+        final String sPass = Intermediates.encodeToURL(pass.getText().toString().trim());
+        final String sName = Intermediates.encodeToURL(name.getText().toString().trim());
+        final String sLastname = Intermediates.encodeToURL(lastname.getText().toString().trim());
 
-        if(TextUtils.isEmpty(sEmail)) {
+        if (TextUtils.isEmpty(sEmail)) {
             Toast.makeText(getApplicationContext(), "Please enter email", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(TextUtils.isEmpty(sPass)) {
+        if (TextUtils.isEmpty(sPass)) {
             Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(sName)) {
+            Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(sLastname)) {
+            Toast.makeText(getApplicationContext(), "Please enter your lastname", Toast.LENGTH_SHORT).show();
             return;
         }
 
         progressDialog.setMessage("Registering...");
         progressDialog.show();
 
+
+
         firebaseAuth.createUserWithEmailAndPassword(sEmail, sPass)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()) {
-                    startActivity(new Intent(getApplicationContext(), MyProfile.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                } else {
-                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            finish();
+                            new GetRequest("http://195.88.209.17/app/in/user.php?name=" + sName + "%20" + sLastname + "&photo=mmbuser.jpg&email=" + sEmail).execute();
+                            startActivity(new Intent(getApplicationContext(), MyProfile.class)
+                                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    .putExtra("Name", sName + " " + sLastname)
+                                    .putExtra("PhotoURL", "mmbuser.jpg")
+                                    .putExtra("E-mail", sEmail));
+                            Storage.addString("Name", name.getText().toString().trim() + " " + lastname.getText().toString().trim());
+                            Storage.addString("E-mail", email.getText().toString().trim());
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/appcorp/mmb/classes/Intermediates.java
+++ app/src/main/java/appcorp/mmb/classes/Intermediates.java
@@ -9,8 +9,13 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Intermediates {
 
@@ -43,4 +48,94 @@ public class Intermediates {
         textView.setText(r);
         return textView.getText().toString();
     }
+
+    public static String encodeToURL(String inString) {
+        String outString = "";
+        try {
+            outString = URLEncoder.encode(inString, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return outString;
+    }
+
+    /*public static String convertRuToEn(String inString) {
+        String outString = "";
+        inString = inString.toLowerCase();
+        inString = inString.replace("а", "_a_");
+        inString = inString.replace("б", "_b_");
+        inString = inString.replace("в", "_v_");
+        inString = inString.replace("г", "_g_");
+        inString = inString.replace("д", "_d_");
+        inString = inString.replace("е", "_e_");
+        inString = inString.replace("ё", "_yo_");
+        inString = inString.replace("ж", "_zh_");
+        inString = inString.replace("з", "_z_");
+        inString = inString.replace("и", "_i_");
+        inString = inString.replace("й", "_ii_");
+        inString = inString.replace("к", "_k_");
+        inString = inString.replace("л", "_l_");
+        inString = inString.replace("м", "_m_");
+        inString = inString.replace("н", "_n_");
+        inString = inString.replace("о", "_o_");
+        inString = inString.replace("п", "_p_");
+        inString = inString.replace("р", "_r_");
+        inString = inString.replace("с", "_s_");
+        inString = inString.replace("т", "_t_");
+        inString = inString.replace("у", "_u_");
+        inString = inString.replace("ф", "_f_");
+        inString = inString.replace("х", "_kh_");
+        inString = inString.replace("ц", "_tc_");
+        inString = inString.replace("ч", "_ch_");
+        inString = inString.replace("ш", "_sh_");
+        inString = inString.replace("щ", "_shch_");
+        inString = inString.replace("ь", "_bbb_");
+        inString = inString.replace("ы", "_y_");
+        inString = inString.replace("ъ", "_bbbb_");
+        inString = inString.replace("э", "_ye_");
+        inString = inString.replace("ю", "_yu_");
+        inString = inString.replace("я", "_ya_");
+        outString = inString;
+        return outString;
+    }
+
+    public static String convertEnToRu(String inString) {
+        String outString = "";
+        inString = inString.toLowerCase();
+        inString = inString.replace("_a_", "а");
+        inString = inString.replace("_b_", "б");
+        inString = inString.replace("_v_", "в");
+        inString = inString.replace("_g_", "г");
+        inString = inString.replace("_d_", "д");
+        inString = inString.replace("_e_", "е");
+        inString = inString.replace("_yo_", "ё");
+        inString = inString.replace("_zh_", "ж");
+        inString = inString.replace("_z_", "з");
+        inString = inString.replace("_i_", "и");
+        inString = inString.replace("_ii_", "й");
+        inString = inString.replace("_k_", "к");
+        inString = inString.replace("_l_", "л");
+        inString = inString.replace("_m_", "м");
+        inString = inString.replace("_n_", "н");
+        inString = inString.replace("_o_", "о");
+        inString = inString.replace("_p_", "п");
+        inString = inString.replace("_r_", "р");
+        inString = inString.replace("_s_", "с");
+        inString = inString.replace("_t_", "т");
+        inString = inString.replace("_u_", "у");
+        inString = inString.replace("_f_", "ф");
+        inString = inString.replace("_kh_", "х");
+        inString = inString.replace("_tc_", "ц");
+        inString = inString.replace("_ch_", "ч");
+        inString = inString.replace("_sh_", "ш");
+        inString = inString.replace("_shch_", "щ");
+        inString = inString.replace("_bbb_", "ь");
+        inString = inString.replace("_y_", "ы");
+        inString = inString.replace("_bbbb_", "ъ");
+        inString = inString.replace("_ye_", "э");
+        inString = inString.replace("_yu_", "ю");
+        inString = inString.replace("_ya_", "я");
+        outString = inString;
+        return outString;
+    }*/
 }
